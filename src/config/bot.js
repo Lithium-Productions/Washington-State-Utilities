60d7d0d134fa7bd7e5a70b0a9777b51bbf09e13b
Lithium-Productions <cheesemanr2@gmail.com>
2026-09-19 14:40:19 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Washington D.C. State Roleplay", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        state: "watching",     // this is what people actually see
         type: 3,               // Custom
       },
     ],
